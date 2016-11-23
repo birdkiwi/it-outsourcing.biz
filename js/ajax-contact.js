81ea75d59454f4e6ebac0f22b019ff1957b5c2a7
Roman Avr <rosin2134@gmail.com>
2016-11-23 21:01:33 +0300

    Страничка "Аренда серверов" обновлена; -Добавлена таблица; Обновлена AJAX-форма обратной связи;

--- js/ajax-contact.js
+++ js/ajax-contact.js
@@ -18,23 +18,33 @@ $(function () {
         $.ajax({
                 type: 'POST',
                 url: $form.attr('action'),
-                data: formData
+                data: formData,
+                dataType: 'json',
+                cache: false
             })
             .done(function (response) {
-                alert("Спасибо за Вашу заявку, в скором времени наши менеджеры свяжутся с Вами!");
-                // Make sure that the formMessages div has the 'success' class.
-                $formMessages.removeClass('error');
-                $formMessages.addClass('success');
+                console.log(response);
+                if(response) {
+                    if(!response.error && response.message) {
+                        $formMessages.removeClass('error');
+                        $formMessages.addClass('success');
 
-                // Set the message text.
-                $formMessages.text('Благодарим вас, ваше сообщение было успешно отправлено!');
+                        // Set the message text.
+                        $formMessages.text('Благодарим вас, ваше сообщение было успешно отправлено!');
+                        alert(response.message);
+                        // Clear the form.
+                        $form[0].reset();
+                    }
+                }
+                else {
+                    alert("Ой! Кажется что-то пошло не так. Пожалуйста, попробуйте еще раз отравить сообщение, либо свяжитесь с нами по телефону.");
+                    $formMessages.removeClass('success');
+                    $formMessages.addClass('error');
+                    $formMessages.text('Попробуйте еще раз отправить сообщение, либо свяжитесь с нами по телефону.');
+                }
 
-                // Clear the form.
-                $form[0].reset();
             })
             .fail(function (data) {
-                // Make sure that the formMessages div has the 'error' class.
-                console.log($form[0]);
                 alert("Ой! Кажется что-то пошло не так. Пожалуйста, попробуйте еще раз отравить сообщение, либо свяжитесь с нами по телефону.");
                 $formMessages.removeClass('success');
                 $formMessages.addClass('error');
